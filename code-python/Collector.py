66a9d93936fd87d7ccbb6adbf4585652d13832ec
paikwiki <paikwiki@gmail.com>
2021-05-20 22:00:09 +0900

    Create Collector

---
+++ code-python/Collector.py
@@ -0,0 +1,22 @@
+import os
+
+from time import localtime, strftime
+from conf.Config import Config
+
+class Collector:
+	currentDate = strftime("%Y-%m-%d", localtime())
+	fileDir = Config().DIR_TO_SAVE
+	fileName = "log_" +  currentDate + ".log"
+	filePath = "/".join([fileDir, fileName])
+
+	def __init__(self):
+		os.makedirs(self.fileDir, exist_ok=True)
+	def getTimestamp(self):
+		t = localtime()
+		cDate = strftime("%Y-%m-%d", t)
+		cTime = strftime("%H:%M:%S", t)
+		return (" ".join([cDate, cTime]))
+	def collect(self):
+		f = open(self.filePath, 'a')
+		f.write(self.getTimestamp() + " hey\n")
+		f.close()
--- code-python/main.py
+++ code-python/main.py
@@ -1,21 +1,7 @@
-import os
+from time import sleep
+from Collector import Collector
 
-from time import localtime, strftime
-from conf.Config import Config
-
-config = Config()
-
-t = localtime()
-
-currentDate = strftime("%Y-%m-%d", t)
-currentTime = strftime("%H:%M:%S", t)
-
-fileDir = config.DIR_TO_SAVE
-fileName = "log_" +  currentDate + ".log"
-filePath = "/".join([fileDir, fileName])
-
-os.makedirs(fileDir, exist_ok=True)
-
-f = open(filePath, 'a')
-f.write(currentDate + " " + currentTime + " hey\n")
-f.close()
+collector = Collector()
+while True:
+	collector.collect()
+	sleep(1)

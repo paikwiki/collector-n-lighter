bd399a3417b160307efa81ba41cede68a13b8560
paikwiki <paikwiki@gmail.com>
2021-06-06 23:07:12 +0900

    Implement lighter
    - lighter can get the latest log file
    - lighter can check log file line by line and
    - put "light on" message on stdout when it is the time to turn it on

--- lighter/Lighter.py
+++ lighter/Lighter.py
@@ -1,34 +1,69 @@
-from time import localtime, strftime, struct_time
+import os
+from time import localtime, strftime, struct_time, mktime, time
 from conf.Config import Config
+from time import sleep
+import datetime
 
 class Lighter:
+    targetFile = NotImplemented
+
     __filePath: str = ""
-    __timestamp: str = ""
+    __days_ago: int = 0
 
     def __init__(self):
-
         self.__setFilePath()
 
     def light(self):
-        self.__setFilePath()
-        self.__setTimestamp()
-        print("Lighter will read from", self.__filePath, ".")
-        print("Light is turned on if there is a", self.__timestamp, "log.")
 
-    # __setTimestamp()
-    # - example: 2021-05-20 22:51:05
-    # TODO: filePath의 날짜와 현재시간을 조합하여 시간을 반환하도록 변경
-    def __setTimestamp(self):
-        t: struct_time = localtime()
-        _cDate: str = strftime("%Y-%m-%d", t)
-        _cTime: str = strftime("%H:%M:%S", t)
-        self.__timestamp = " ".join([_cDate, _cTime])
+        # 파일 열기
+        if (self.__filePath != ""):
+            self.targetFile = open(self.__filePath)
+        if not self.targetFile: return
+
+        line = self.targetFile.readline().rstrip()
+
+        # 빈 파일일 경우 끝내기
+        if not line:
+            self.targetFile.close()
+            return
+        curr = datetime.datetime.now() - datetime.timedelta(days=self.__days_ago)
+        seconds = mktime(datetime.datetime.strptime(line, "%Y-%m-%d %H:%M:%S").timetuple())
+        parsedLine = datetime.datetime.fromtimestamp(seconds)
+
+        # 지나간 로그를 스킵하고 타겟이 되는 로그를 가져옴
+        # 타겟이 되는 로그의 조건: parsedLine이 curr보다 크거나 같은 경우
+        while (parsedLine < curr):
+            line = self.targetFile.readline().rstrip()
+            if not line: break # 더이상 읽어올 게 없거나 빈 줄을 만나면 while 종료
+
+            seconds = mktime(datetime.datetime.strptime(line, "%Y-%m-%d %H:%M:%S").timetuple())
+            parsedLine = datetime.datetime.fromtimestamp(seconds)
+        print(parsedLine, " - ", curr, " = ", parsedLine - curr)
+
+        # 타겟이 되는 로그가 불을 켜야하는 상태인지 체크
+        if (parsedLine >= curr and parsedLine <= curr + datetime.timedelta(seconds=Config.DELAY_DEFAULT + 0.1)): # TODO: 혹시 몰라서 패딩값 적용
+            print(">> Light ON")
+            sleep(Config.DELAY_LIGHT_ON)
+            print("<< Light OFF")
+
+        # 파일 닫기
+        self.targetFile.close()
 
     # __setFilePath()
     # - example: ./logs/log_2021-05-20.log
-    # TODO: 가장 최신의 파일을 가져오도록 변경
+    # 가장 최신의 파일을 가져오도록 변경. Config.RANGE_TO_FIND의 날짜만큼 탐색
     def __setFilePath(self):
-        currentDate = strftime("%Y-%m-%d", localtime())
-        _fileDir: str = Config.DIR_LOGS
-        _fileName: str = "log_" + currentDate + ".log"
-        self.__filePath = "/".join([_fileDir, _fileName])
+        currentDate = datetime.datetime.now()
+        fileDir: str = Config.DIR_LOGS
+        for days_ago in range(1, Config.RANGE_TO_FIND):
+            fileDate = currentDate - datetime.timedelta(days=days_ago)
+            fileName: str = "log_" + fileDate.strftime("%Y-%m-%d") + ".log"
+            filePath: str = "/".join([fileDir, fileName])
+            if os.path.isfile(filePath):
+                self.__filePath = filePath
+                self.__days_ago = days_ago
+                break
+        if (self.__filePath == ""):
+            print("Error: Log file does not exist.")
+        else:
+            print(self.__filePath)
--- lighter/conf/Config.py
+++ lighter/conf/Config.py
@@ -2,6 +2,8 @@ class Config:
     # will store logs(one file per a day)
     DIR_LOGS = "./logs"
     # unit: second
-    DELAY_SIGNAL_IN = 5
+    DELAY_LIGHT_ON = 5
     # unit: second
     DELAY_DEFAULT = 1
+    # unit: days
+    RANGE_TO_FIND = 7

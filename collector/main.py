7fae7c038e4f03313946db5171b1e2c89957d7a9
paikwiki <paikwiki@gmail.com>
2021-06-26 21:45:27 +0900

    End loop at ENDTIME_OF_DAY

--- collector/main.py
+++ collector/main.py
@@ -1,14 +1,23 @@
 import os
 from typing import Any
-from time import sleep
+from time import sleep, localtime, strftime, strptime
+# from datetime import datetime
 from Collector import Collector
 from conf.Config import Config
 
-collector : Any = Collector()
-readFile : str = Config.FILE_TO_READ
-lastmod : int = int(os.path.getmtime(readFile))
+collector = Collector()
+readFile = Config.FILE_TO_READ
+lastmod = int(os.path.getmtime(readFile))
 
-while True:
+def isEnd():
+    currentTime = localtime()
+    currentDate = strftime("%Y-%m-%d", currentTime)
+    string = " ".join([currentDate, Config.ENDTIME_OF_DAY])
+    tm = strptime(string, '%Y-%m-%d %H:%M:%S')
+    if tm < currentTime:
+        return True
+    return False
+while (not isEnd()):
     if lastmod != int(os.path.getmtime(readFile)):
         collector.collect()
         sleep(Config.DELAY_SIGNAL_IN)
